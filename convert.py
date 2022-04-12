a79d149fe9f6f3909b811dbade5beb11c3683c02
Devin Lange <devinscottlange@gmail.com>
2022-04-12 16:50:50 -0600

    convert to data and edges csv files

--- convert.py
+++ convert.py
@@ -20,18 +20,36 @@ def main():
     id = 'CellNum'
     parent = 'ParentCell'
     data_arrays = []
+    edges = []
     for cell in cell_data_list:
-        data_length = len(cell[time_key])
-        id_col = np.full((data_length, 1), cell[id])
+        cell_id = cell[id][0][0]
+        parent_id = cell[parent]
+        if len(parent_id) > 0:
+            parent_id = parent_id[0][0]
+        else:
+            parent_id = -1
+        edges.append([cell_id, parent_id])
 
+        data_length = len(cell[time_key])
+        id_col = np.full((data_length, 1), cell[id], dtype='uint')
         values = [id_col]
         for key in keys:
             values.append(cell[key])
         combined = np.array(values).T[0]
         data_arrays.append(combined)
 
+
+
     data_out = np.concatenate(data_arrays, axis=0)
-    # print(data_out.shape)
+    column_names = ['id']
+    column_names.extend(keys)
+    df = pd.DataFrame(data_out, columns=column_names)
+    df['id'] = df['id'].astype(int)
+    df.to_csv(OUT_FOLDER + 'data.csv', index=False, header=True)
+
+    df = pd.DataFrame(edges, columns=['id','parent']).astype(int)
+    df.to_csv(OUT_FOLDER + 'edges.csv', index=False, header=True)
+
 
     return
 
