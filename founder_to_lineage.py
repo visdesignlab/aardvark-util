bcfa28dce53481f7a1e16047f3cff566b9fe2bd9
Devin Lange <devinscottlange@gmail.com>
2023-03-29 10:51:38 -0600

    refactor with high-level aproach decided

--- founder_to_lineage.py
+++ founder_to_lineage.py
@@ -7,34 +7,28 @@ import polars as pl
 IN_FOLDER = './in/'
 OUT_FOLDER = './out/'
 
+def updateParentLookup(lookup, df):
+    return
 
 def main():
     filename = 'R3_D10_16_cell104.csv'
     df = pl.read_csv(IN_FOLDER + filename, dtypes={'mCherry - Minimum Intensity ()': pl.Float64}, null_values=['-∞', '∞'])
-    print(df)
-    df = df.filter(pl.col('Tracking ID') != pl.col('Lineage ID'))
-    print(df)
-    grouped = df.groupby('Tracking ID').agg(
-        [
-            pl.col('Frame').min().alias('Frame.min'),
-            pl.col('Frame').max().alias('Frame.max'),
-            pl.col('Lineage ID').min() # They should all be the same.
-        ])
-    grouped = grouped.sort('Frame.min')
-    
-    print(grouped.head(5))
-    print(grouped.tail(5))
-
-    # for name, data in grouped:
-    #     data.with_columns()
-    #     data.row(1)['Frame'] = 66        
-    #     print(name)
-    #     print(data)
-    # df = df[df['Tracking ID'] != df['Lineage ID']]
     # print(df)
-    # df_grouped = df.groupby('Tracking ID')
-    # df_grouped['Frame'] = 6
-    # print(df_grouped)
+    parentLookupDF = df.select(pl.col('Tracking ID'), (pl.col('Lineage ID')).alias('Parent ID')).unique()
+    # print(parentLookupDF)
+    parentLookup = {}
+    for row in parentLookupDF.iter_rows():
+        parentLookup[row[0]] = row[1]
+    # print(parentLookup)
+    grouped = df.groupby('Lineage ID')
+    # print(grouped)
+    for name, data in grouped:
+        updateParentLookup(parentLookup, data)
+
+    parentLookupDF = pl.DataFrame({'Tracking ID': parentLookup.keys(), 'Parent ID': parentLookup.values()})
+    df = df.join(parentLookupDF, on='Tracking ID')
+    print(df)
+    df.write_csv(OUT_FOLDER + filename)
     return
 
 if __name__ == '__main__':
